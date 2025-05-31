1641e822db80e7105e22ee312c6ee5f1df85f85b
hyh050 <141797347+hyh050@users.noreply.github.com>
2025-05-31 15:56:02 +0900

    키보드 조작 기능 추가 (이슈 #1 해결)

--- main.js
+++ main.js
@@ -57,3 +57,21 @@ function move(direction) {
 }
 
 drawMaze();
+
+// === 키보드 조작 추가 ===
+window.addEventListener('keydown', function(event) {
+  switch(event.key) {
+    case 'ArrowUp':
+      move('up');
+      break;
+    case 'ArrowDown':
+      move('down');
+      break;
+    case 'ArrowLeft':
+      move('left');
+      break;
+    case 'ArrowRight':
+      move('right');
+      break;
+  }
+});
